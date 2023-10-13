f6aea8d4330e0cc34f4c89d52ee0fe7a2d2b3ee2
nandan-gaurav-95 <121486188+nandan-gaurav-95@users.noreply.github.com>
2023-10-13 11:39:05 +0530

    Counter added

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -25,7 +25,11 @@ const Feature = () => {
                     <div className="btn-lg-square bg-primary rounded-circle me-3">
                         <i className="fa fa-check text-white"></i>
                     </div>
-                    <h1 className="mb-0" data-toggle="counter-up">1</h1>
+                    <CountUp end={1} duration={3} separator="," decimals={0}>
+                {({ countUpRef }) => (
+                  <h1 className="mb-0" ref={countUpRef} />
+                )}
+              </CountUp>
                 </div>
                 
                 <h5 className="mb-3">MWp Installed</h5>
@@ -36,21 +40,31 @@ const Feature = () => {
                     <div className="btn-lg-square bg-primary rounded-circle me-3">
                         <i className="fa fa-award text-white"></i>
                     </div>
-                    <h1 className="mb-0" data-toggle="counter-up">3123</h1>
+
+                    <CountUp end={50} duration={3} separator="," decimals={0}>
+                {({ countUpRef }) => (
+                  <h1 className="mb-0" ref={countUpRef} />
+                )}
+              </CountUp>
+                    
                 </div>
-                <h5 className="mb-3">Awards Win</h5>
-                <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
+                <h5 className="mb-3">Happy Clients</h5>
+                {/* <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span> */}
             </div>
-            <div className="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
+            {/* <div className="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
                 <div className="d-flex align-items-center mb-4">
                     <div className="btn-lg-square bg-primary rounded-circle me-3">
                         <i className="fa fa-users-cog text-white"></i>
                     </div>
-                    <h1 className="mb-0" data-toggle="counter-up">1831</h1>
+                    <CountUp end={1831} duration={2} separator="," decimals={0}>
+                {({ countUpRef }) => (
+                  <h1 className="mb-0" ref={countUpRef} />
+                )}
+              </CountUp>
                 </div>
                 <h5 className="mb-3">Expert Workers</h5>
                 <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
-            </div>
+            </div> */}
         </div>
     </div>
 </div>
