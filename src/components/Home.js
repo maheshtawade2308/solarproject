dc7bd21ea0f9e9f9f19a8c566dff0823abd0d7d8
Mahesh Tawade <maheshtawade2308@gmail.com>
2023-10-22 14:57:33 +0530

    unwanted pages deleted, carousel images size decreased, certificate added

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,6 @@ const Home = () =>{
         <div>
             <Navbar/>
            <ImageCarousel/>
-            {/* <Header/> */}
             <Feature/>
             <Products/>
             <AboutHome/>
--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Image1 from "./assets/archanadip1.jpg";
 import Image2 from "./assets/archanadip2.jpg";
+import Image3 from "./assets/certificate.jpeg";
 
 const ImageCarousel = () => {
 
@@ -10,17 +11,21 @@ const ImageCarousel = () => {
     autoPlay: true, // Enable auto-play
     infiniteLoop: true,
     showThumbs: false,
-    stopOnHover: false, // Set to false to allow one-direction sliding
+    transitionEffect: "fade",
+    stopOnHover: true, // Set to false to allow one-direction sliding
     interval: 3000, // Set the sliding time to 3 seconds (3000 milliseconds)
   };
   
   return (
     <Carousel {...carouselSettings} >
-      <div>
-        <img src={Image1} alt="Image 1" />
+      <div >
+        <img className='w-75 h-25 d-inline-block' src={Image1} alt="Image 1" />
       </div>
       <div>
-        <img src={Image2} alt="Image 2" />
+        <img className='w-75 h-25 d-inline-block' src={Image2} alt="Image 2" />
+      </div>
+      <div>
+        <img className='w-75 h-25 d-inline-block' src={Image3} alt="Image 2" />
       </div>
     </Carousel>
   );
